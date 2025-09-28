de6cc9957895c8b76552d6237f7860035f3657bb
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-09-28 17:48:32 +0700

    แก้ไข major แก้ไข font เพิ่ม footer

--- app/major/page.tsx
+++ app/major/page.tsx
@@ -1,16 +1,27 @@
 import MenuBar from "@/components/MenuBar";
 import MenuMajor from "@/components/MenuMajor";
+import { Itim, Bungee } from "next/font/google";
+
+const itim = Itim({
+  subsets: ["thai"],
+  weight: ["400"],
+}); 
+
+const bungee = Bungee({
+  subsets: ["latin"],
+  weight: ["400"],
+}); 
 
 export default function Page() {
   return (
     <>
         <MenuBar />
-        <h1 className="text-center">สาขาวิชาต่างๆ ในมหาวิทยาลัย</h1>
+        <h1 className={`${itim.className} text-center`}>สาขาวิชาต่างๆ ในมหาวิทยาลัย</h1>
         {/* --------ใส่โค้ดแสดงเมนูไปยังสาขาวิชาต่างๆ--------- */}
         <MenuMajor />
         {/* -------------------------------------------- */}
         <hr />
-        <p>
+        <p className={`${bungee.className}`}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio consectetur harum dignissimos nam veritatis quidem expedita aut accusantium provident eos, earum quasi non tempora aliquam, quos tenetur, corporis sit id!
         </p>
     </>
